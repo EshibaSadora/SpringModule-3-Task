1acf34b9ace623a19125996457967edd41c80436
EshibaSadora <egorshornikov@mail.ru>
2022-08-31 13:18:43 +0300

    CreateProject

---
+++ src/test/java/ru/shornikov/ShornikovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shornikov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShornikovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
